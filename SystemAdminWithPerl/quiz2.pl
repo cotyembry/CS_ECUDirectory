6ee19b1b5eaf4264a17010a9ed9e593701697784
Embry <johcemb@cs.ecok.edu>
2017-06-13 15:59:19 -0500

    added SystemAdminWithPerl

---
+++ SystemAdminWithPerl/quiz2.pl
@@ -0,0 +1,14 @@
+#!/usr/local/bin/perl -w
+
+$y = "ECU";
+$w = "Tigers";
+
+$z = "East Central University Computer Science Department";
+
+print "$y$w\n";
+
+@l = ("Name", "Age", "Occupation");
+print"@l\n";
+print"enter something\n";
+$in = <STDIN>;
+print"$in<-\n";
